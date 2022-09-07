980b0ac09e37bc064df58830afdaf63b8c3ff30e
Sheikh Faiyaz Ahmed <faiyazahmedsamin@gmail.com>
2022-09-07 14:27:50 +0600

    Remove unused files

--- cypress/integration/1-getting-started/test1_spec.js
+++
@@ -1,7 +0,0 @@
-describe('My first test suite', function() {
-
-    it('My first test', function()
-    {
-        cy.visit('https://docs.cypress.io/guides/getting-started/writing-your-first-test#Write-your-first-test');
-    })
-})
